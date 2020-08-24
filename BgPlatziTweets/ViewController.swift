0812e54c08dd0b1cf8581df8d4c10c9b055bef62
Bryan Gomez <bryan12gh@gmail.com>
2020-08-24 15:49:28 -0500

    Initial Commit

---
+++ BgPlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BgPlatziTweets
+//
+//  Created by Bryan Andres Gomez Hernandez on 8/24/20.
+//  Copyright © 2020 Bryan Andres Gomez Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
